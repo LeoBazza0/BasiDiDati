92173497257cd415254360dfe143b1c0387162a4
LeoBazza <119495200+LeoBazza0@users.noreply.github.com>
2024-07-06 19:38:33 +0200

    Delete ProgettoBazza directory
    Wasn't the final one

--- ProgettoBazza/SQL/querySQL.sql
+++
@@ -1,3 +0,0 @@
-/* da inserirci tutte le query necessarie */
-
-
--- ProgettoBazza/modules/vars.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$DEFAULT_DIR = "/progettoBazza"
-
-// xampp/htdocs/proettoBazza
-
-$CONNECTION_STRING = "host=localhost port=5432 dbname=postgres user=postgres password=unimi"
